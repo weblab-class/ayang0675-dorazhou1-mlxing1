6e3dd401bd1d85a6cf27a52c8a6cd0a90a93ad0b
dorazhou1 <64435558+dorazhou1@users.noreply.github.com>
2024-01-26 15:21:24 -0500

    glowy

--- client/src/components/board/pieces/Piece.js
+++ client/src/components/board/pieces/Piece.js
@@ -1,6 +1,7 @@
 import React from "react"
-const Piece = ({rank,file,piece}) =>{
-    return (<div className = {`piece ${piece} p-${rank}${file}`} draggable={true}>
+
+const Piece = ({rank,file,piece,entangled}) =>{
+    return (<div className = {`piece ${piece} p-${rank}${file}`} draggable={true}><img className={`piece-img  ${entangled}`} src={`pieces/${piece}.svg`} />
     </div>)
 }
 export default Piece
--- client/src/components/board/pieces/pieces.js
+++ client/src/components/board/pieces/pieces.js
@@ -1,8 +1,43 @@
 import "./pieces.css"
 import React from "react"
 import Piece from './Piece.js'
+
+function getEntangled() {
+    
+    // choose entangled 
+    let entangled = new Array(8).fill('').map(x=>new Array(8).fill(''))
+
+    const rank = [0,1];
+    const file = [0,1,2,3,4,5,6,7];
+    let pairs = []
+    rank.forEach( (r) => {
+        file.forEach( (f) => {
+            pairs.push([r,f])
+        })
+    });
+    let l = 16;
+
+
+    for(let i=0; i<3; i++) {
+        let first = Math.floor(Math.random() * l);
+        let firstpair = pairs[first]
+        pairs.splice(first, 1);
+        l--;
+        let secondpair = pairs[Math.floor(Math.random() * l)];
+        l--;
+        // whites
+        entangled[firstpair[0]][firstpair[1]] = 'entangled-w-'+i;
+        entangled[secondpair[0]][secondpair[1]] = 'entangled-w-'+i;
+        // blacks
+        entangled[7-firstpair[0]][firstpair[1]] = 'entangled-b-'+i;
+        entangled[7-secondpair[0]][secondpair[1]] = 'entangled-b-'+i;
+    }
+
+    return entangled
+}
+
 const Pieces = () => {
-    const position = new Array(8).fill('').map(x=>new Array(8).fill(''))
+    let position = new Array(8).fill('').map(x=>new Array(8).fill(''))
     position[0][0]='wr'; 
     position[0][1]='wh';
     position[0][2]='wb';
@@ -38,6 +73,10 @@ const Pieces = () => {
     position[6][0]='bp';
 
     console.log(position);
+
+    const entangled = getEntangled()
+    console.log(entangled[0][1])
+    console.log(entangled)
     
     return <div className = 'pieces'>
         {position.map((r,rank) => r.map((f,file) => position[rank][file] ? 
@@ -45,7 +84,8 @@ const Pieces = () => {
         key = {rank + '-' + file}
         rank = {rank}
         file = {file}
-        piece = {position[rank][file]}/>
+        piece = {position[rank][file]}
+        entangled = {entangled[rank][file]}/>
         : null))}
     </div>
 }
