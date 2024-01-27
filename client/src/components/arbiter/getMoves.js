c65aad15fdf5a19b2aa58e04480b1518bdfa2b6a
dorazhou1 <64435558+dorazhou1@users.noreply.github.com>
2024-01-26 23:03:11 -0500

    get moves

--- client/src/components/arbiter/getMoves.js
+++ client/src/components/arbiter/getMoves.js
@@ -1,4 +1,4 @@
-export const getRookMoves = ({position,piece,rank,file}) =>{
+const getRookMoves = ({position,piece,rank,file}) => {
     const moves =[]
     const us = piece[0]
     const enemy = us=='w'?'b':'w'
@@ -14,11 +14,11 @@ export const getRookMoves = ({position,piece,rank,file}) =>{
             const x= rank + (i*dir[0])
             const y= file + (i*dir[1])
             if(position?.[x]?.[y] == undefined) break
-            if(position[x][y].startsWith(enemy)){
+            if(position[x][y].startsWith(enemy)) {
                 moves.push([x,y])
                 break;
             }
-            if(position[x][y].startsWith(us)){
+            if(position[x][y].startsWith(us)) {
                 break;
             }
             moves.push([x,y])
@@ -26,4 +26,183 @@ export const getRookMoves = ({position,piece,rank,file}) =>{
     })
     console.log(moves)
     return moves
+}
+
+const getBishopMoves = ({position,piece,rank,file}) => {
+    const moves =[]
+    const us = piece[0]
+    const enemy = us=='w'?'b':'w'
+
+    const direction = [
+        [1,1],
+        [1,-1],
+        [-1,1],
+        [-1,-1],
+    ]
+    direction.forEach(dir => {
+        for(let i=1;i<8; i++){
+            const x= rank + (i*dir[0])
+            const y= file + (i*dir[1])
+            if(position?.[x]?.[y] == undefined) break
+            if(position[x][y].startsWith(enemy)) {
+                moves.push([x,y])
+                break;
+            }
+            if(position[x][y].startsWith(us)) {
+                break;
+            }
+            moves.push([x,y])
+        }
+    })
+    console.log(moves)
+    return moves
+}
+
+
+const getKnightMoves = ({position,piece,rank,file}) => {
+    const moves =[]
+    const us = piece[0]
+    const enemy = us=='w'?'b':'w'
+
+    const direction = [
+        [1,2],
+        [1,-2],
+        [-1,2],
+        [-1,-2],
+        [2,1],
+        [2,-1],
+        [-2,1],
+        [-2,-1]
+    ]
+    direction.forEach(dir => {
+        moves.push([rank + dir[0], file + dir[1]])
+    })
+    console.log(moves)
+    return moves
+}
+
+const getPawnMoves = ({position,piece,rank,file}) => {
+    const moves =[]
+    const us = piece[0]
+    const enemy = us=='w'?'b':'w'
+
+    if(position[rank+1][file] == "") {
+        moves.push([rank+1,file]);
+        if(position[rank+2][file] == "") {
+            moves.push([rank+2,file]);
+        }
+    }
+
+    if(position[rank+1][file+1].startsWith(enemy)) {
+        moves.push([rank+1,file+1]);
+    }
+
+    if(position[rank+1][file-1].startsWith(enemy)) {
+        moves.push([rank+1,file-1]);
+    }
+    
+    return moves
+}
+
+const getQueenMoves = ({position,piece,rank,file}) => {
+    const moves =[]
+    const us = piece[0]
+    const enemy = us=='w'?'b':'w'
+
+    const direction = [
+        [1,1],
+        [1,-1],
+        [-1,1],
+        [-1,-1],
+        [-1,0],
+        [1,0],
+        [0,-1],
+        [0,1],
+    ]
+    direction.forEach(dir => {
+        for(let i=1;i<8; i++){
+            const x= rank + (i*dir[0])
+            const y= file + (i*dir[1])
+            if(position?.[x]?.[y] == undefined) break
+            if(position[x][y].startsWith(enemy)) {
+                moves.push([x,y])
+                break;
+            }
+            if(position[x][y].startsWith(us)) {
+                break;
+            }
+            moves.push([x,y])
+        }
+    })
+    console.log(moves)
+    return moves
+}
+
+const getKingMoves = ({position,piece,rank,file}) => {
+    
+    const moves =[]
+    const us = piece[0]
+    const enemy = us=='w'?'b':'w'
+
+    const direction = [
+        [1,1],
+        [1,-1],
+        [-1,1],
+        [-1,-1],
+        [-1,0],
+        [1,0],
+        [0,-1],
+        [0,1],
+    ]
+
+    const getEnemyMoves = () => {
+        enemyMoves = []
+        const findPiece = (piece) => {
+            pieces = []
+            for(let i=0;i<8; i++) {
+                for(let j=0;j<8;j++) {
+                    if(position[i][j]==piece) {
+                        pieces.push([i,j]);
+                    }
+                }
+            }
+        }
+        bishopPos = findPiece(enemy+'b')
+        bishopPos.forEach(pos => {
+            enemyMoves.append(getBishopMoves(position, enemy+'b',pos[0], pos[1]))
+        })
+        knightPos = findPiece(enemy+'h')
+        knightPos.forEach(pos => {
+            enemyMoves.append(getBishopMoves(position, enemy+'h',pos[0], pos[1]))
+        })
+        kingPos = findPiece(enemy+'k')
+        kingPos.forEach(pos => {
+            enemyMoves.append(getBishopMoves(position, enemy+'k',pos[0], pos[1]))
+        })
+        pawnPos = findPiece(enemy+'p')
+        pawnPos.forEach(pos => {
+            enemyMoves.append(getBishopMoves(position, enemy+'p',pos[0], pos[1]))
+        })
+        queenPos = findPiece(enemy+'q')
+        queenPos.forEach(pos => {
+            enemyMoves.append(getBishopMoves(position, enemy+'q',pos[0], pos[1]))
+        })
+        rookPos = findPiece(enemy+'r')
+        rookPos.forEach(pos => {
+            enemyMoves.append(getBishopMoves(position, enemy+'r',pos[0], pos[1]))
+        })
+        return enemyMoves
+    }
+
+    enemyMoves = getEnemyMoves()
+
+    direction.forEach( dir => {
+        if(!position[rank+dir[0]][file+dir[1]].startsWith(us) && !enemyMoves.includes([rank+dir[0],file+dir[1]])) {
+            moves.push([rank+1,file+1]);
+        }
+    })
+}
+
+export const getMoves = ({position,piece,rank,file}) => {
+    return getRookMoves({position,piece,rank,file});
 }
